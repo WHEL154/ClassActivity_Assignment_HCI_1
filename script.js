d3754d1f9f1d0abaaa9b6051ab6df583f5eef6c9
WHEL154 <114067303+WHEL154@users.noreply.github.com>
2022-12-19 23:23:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //Please put your answer here
+$('.fa-times').click(
+  function(){
+  
+    $('.signup-modal-wrapper').fadeOut();
+
+$('.btn').click(
+  function(){
+  
+    $('.signup-modal-wrapper').fadeIn();
+});
+
+});
